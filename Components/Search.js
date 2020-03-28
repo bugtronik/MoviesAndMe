121baa88f4f397cbd039a90ec82140b0b19dfbc6
unknown <bugtronik@gmail.com>
2020-03-28 21:45:11 +0100

    Début des props

--- Components/Search.js
+++ Components/Search.js
@@ -1,18 +1,42 @@
 import React from 'react'
 
 //Importation des component de vue, champ de saisie et bouton
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet,View, TextInput, Button, FlatList, Text } from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
 
 class Search extends React.Component {
   render() {
     return (
       // Ici, on renvoie l'affichage à l'écran
-      <View style={{ marginTop: 20 }}>
-        <TextInput placeholder="Titre du film" style={{ marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5 }} />
+
+      <View style={styles.main_container}>
+        <TextInput placeholder="Titre du film" style={styles.textinput} />
         <Button title="Rechercher" onPress={() => {}} />
+        {
+          <FlatList data={films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem/>} />
+        }
       </View>
+
     )
   }
 }
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
 
 export default Search
